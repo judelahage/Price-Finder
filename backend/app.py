b02d265004e9307567678bfb53c866f5eb087ff8
judelahage <106047025+judelahage@users.noreply.github.com>
2025-03-20 13:37:20 -0400

    worked on setting up database

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+import subprocess
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
 
-@app.route('/')
-def helloWorld():
-    return 'Hello, World!'
+db = SQLAlchemy(app)
 
-if __name__ == "__main__":
-    app.run(debug = True)
+#setting up database
+class ProductResult(db.Model):
+    id = db.Column()
+    name = db.Column(db.String(1000))
+    img = db.Column(db.String(1000))
+    url = db.Column(db.String(1000))
+    price = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    search_text = db.Column(db.String(255))
+    source = db.Column(db.String(255))
+    
+    def init(self, name, img, url, price, search_text, source):
+        self.name = name
+        self.url = url
+        self.img = img
+        self.price = price
+        self.search_text = search_text
+        self.source = source
+        

97b63a249f4449efc9c62b449ff5a44291c04bfb
judelahage <106047025+judelahage@users.noreply.github.com>
2025-03-20 13:51:08 -0400

    created tracked products model, handled sending post requests to the results

--- backend/app.py
+++ backend/app.py
@@ -28,4 +28,34 @@ class ProductResult(db.Model):
         self.price = price
         self.search_text = search_text
         self.source = source
+
+class TrackedProducts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(1000))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    tracked = db.Column(db.Boolean, default = True)
+    
+    def init(self, name, tracked = True):
+        self.name = name
+        self.tracked = tracked
+
+#send post request to results
+@app.route('/results', methods=['POST'])
+def submitResults():
+    results = request.json.get('data')
+    search_text = request.json.get('search_text')
+    source = request.json.get('source')
+
+    for result in results:
+        product_result = ProductResult(
+            name = result['name']
+            url = result['url']
+            img = result['img']
+            price = result['price']
+            search_text = search_text
+            source = source
+        )
+        db.session.add(product_result)
         
+    db.session.commit()
+    return jsonify({'message': 'Received Data Successfully'}), 200
